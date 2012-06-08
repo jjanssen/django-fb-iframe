ef1bd94c14bcac6724bd81b3d4bb8cd79a2e9211
Janneke Janssen <j.janssen@lukkien.com>
2012-06-08 09:44:28 +0200

    I haz hipster setup.py and MANIFEST

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-fb-iframe',
-    version='0.1',
+    version='0.1.1',
     description='A small Django app to prevent CSRF request errors by a Facebook iframe.',
     long_description=open('README.rst').read(),
     author='Janneke Janssen',
     author_email='j.janssen@lukkien.com',
     url='http://github.com/jjanssen/django-fb-iframe',
-    packages=find_packages(),
+    packages=find_packages(exclude=['example_fb_iframe']),
     include_package_data=True
 )
